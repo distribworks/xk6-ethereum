f36f028bebb947cdd10fc955f3d982a340c4c8b2
Victor Castell <0x@vcastellm.xyz>
2024-03-10 18:16:34 +0100

    Fix contract call value

--- contract.go
+++ contract.go
@@ -2,6 +2,7 @@ package ethereum
 
 import (
 	"fmt"
+	"math/big"
 
 	"github.com/umbracle/ethgo"
 	"github.com/umbracle/ethgo/contract"
@@ -13,6 +14,13 @@ type Contract struct {
 	client *Client
 }
 
+type TxnOpts struct {
+	Value    uint64
+	GasPrice uint64
+	GasLimit uint64
+	Nonce    uint64
+}
+
 // Call executes a call on the contract
 func (c *Contract) Call(method string, args ...interface{}) (map[string]interface{}, error) {
 	return c.Contract.Call(method, ethgo.Latest, args...)
@@ -20,13 +28,19 @@ func (c *Contract) Call(method string, args ...interface{}) (map[string]interfac
 
 // Txn executes a transactions on the contract and waits for it to be mined
 // TODO maybe use promise
-func (c *Contract) Txn(method string, opts contract.TxnOpts, args ...interface{}) (string, error) {
+func (c *Contract) Txn(method string, opts TxnOpts, args ...interface{}) (string, error) {
 	txn, err := c.Contract.Txn(method, args...)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to create contract transaction: %w", err)
 	}
 
-	txn.WithOpts(&opts)
+	txo := contract.TxnOpts{
+		Value:    big.NewInt(int64(opts.Value)),
+		GasPrice: opts.GasPrice,
+		GasLimit: opts.GasLimit,
+		Nonce:    opts.Nonce,
+	}
+	txn.WithOpts(&txo)
 
 	err = txn.Do()
 	if err != nil {
